cb1a45ac1a63a260b3d38e4d9ca8e6fa81d358eb
andrewwong415 <140117951+andrewwong415@users.noreply.github.com>
2025-01-13 15:51:03 -0800

    Update nm_pathfinder.py
    Draws a potentially suboptimal path that avoids borders

--- src/nm_pathfinder.py
+++ src/nm_pathfinder.py
@@ -18,8 +18,8 @@ def find_path (source_point, destination_point, mesh):
 
     path = []
     boxes = {}
-
-
+    detail_points = {}
+    
     # identify source and destination boxes
     # mesh returns (box): [(adjacent boxes)]
     source_box = None
@@ -27,7 +27,6 @@ def find_path (source_point, destination_point, mesh):
 
     for box in mesh['boxes']:   #iterate through list of boxes
         x1, x2, y1, y2 = box
-        print(f"box: {box}")
         if x1 <= source_point[0] <= x2 and y1 <= source_point[1] <= y2:    # check if source point is within box
             source_box = box
         if x1 <= destination_point[0] <= x2 and y1 <= destination_point[1] <= y2:    # check if destination point is within box
@@ -40,33 +39,52 @@ def find_path (source_point, destination_point, mesh):
     print(f"Source box: {source_box}")
     print(f"Destination box: {dest_box}") 
 
-    # Frontier for BFS adapted from Amit Patel
+    # Modified Frontier for BFS adapted from Amit Patel
     frontier = Queue()
     frontier.put(source_box)
-    boxes = dict()
-    boxes[source_box] = None
+    came_from = dict()
+    came_from[source_box] = None
+    detail_points[source_box] = source_point    # Line starts at source point
 
     while not frontier.empty():
         current = frontier.get()
         for next in mesh['adj'][current]:
-            if next not in boxes:
+            if next not in came_from:
+                # compute set of legal lines
+                # x range of border = [max(b1x1, b2x1), min(b1x2, b2x2)], y range of border = [max(b1y1, b2y1), min(b1y2, b2y2)]
+                b1x1, b1x2, b1y1, b1y2 = current
+                b2x1, b2x2, b2y1, b2y2 = next
+
+                # valid paths 
+                border_x = [max(b1x1, b2x1), min(b1x2, b2x2)]  
+                border_y = [max(b1y1, b2y1), min(b1y2, b2y2)]
+
+                #current_x, current_y = detail_points[current]
+                #distance = euclidean distance
+
+                next_point = (border_x[0], border_y[0])    # always chooses the first detail_point, not optimized
+                detail_points[next] = next_point
+
                 frontier.put(next)
-                boxes[next] = current
+                came_from[next] = current
 
     # Check if dest_box is reachable
-    if dest_box not in boxes:
+    if dest_box not in came_from:
         print("No path!")
         return None, []
-
+    
     # BFS adapted from Amit Patel
     current = dest_box
-    box_path = []
+    path.append(destination_point)
     while current != source_box:
-        box_path.append(current)
-        current = boxes[current]
-    box_path.append(source_box)
-    box_path.reverse()
+        path.append(detail_points[current])
+        current = came_from[current]
+    path.append(detail_points[source_box])
+    path.reverse()
 
-    path = [source_point, destination_point]
+    print(f"source point: {source_point}")
+    print(f"dest point: {destination_point}")
+
+    boxes = came_from
 
     return path, boxes.keys()
